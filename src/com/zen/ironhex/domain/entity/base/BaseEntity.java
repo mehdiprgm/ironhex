1bd9335bbe4ee5c35d05bb01d6cd8335d59d1a37
mehdiprgm <zendevmehdi@gmail.com>
2026-06-08 13:25:03 +0330

    base entity of database

---
+++ src/com/zen/ironhex/domain/entity/base/BaseEntity.java
@@ -0,0 +1,13 @@
+package com.zen.ironhex.domain.entity.base;
+
+public abstract class BaseEntity {
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
